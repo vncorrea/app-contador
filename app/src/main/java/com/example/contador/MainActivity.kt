ceaeb4d74f5ec5d1780bd4e78e514bd2d63173a7
vncorrea <vinicorreag2001@gmail.com>
2024-08-23 23:58:06 -0300

    feat: logica do contador

--- app/src/main/java/com/example/contador/MainActivity.kt
+++ app/src/main/java/com/example/contador/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.example.contador
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
+        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
+
+        textView.text = count.toString()
+        buttonMinus.isEnabled = false
+
+        buttonMinus.setOnClickListener {
+            count--
+            textView.text = count.toString()
+            buttonMinus.isEnabled = count != 0
+        }
+
+        buttonPlus.setOnClickListener {
+            count++
+            textView.text = count.toString()
+            buttonMinus.isEnabled = true
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
